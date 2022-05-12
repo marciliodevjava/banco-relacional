97a604af620b6201a26da64b93350e2123c928d3
marciliodevjava <marciliodevjava@gmail.com>
2022-05-12 17:00:22 -0300

    Atualização

--- consultarComJoin.sql
+++ consultarComJoin.sql
@@ -1,1 +1,8 @@
 SELECT * FROM estado e, cidades c
+WHERE e.id = c.estado_id;
+
+SELECT 
+    e.nome as estado, 
+    c.nome as cidades, 
+    regiao as Região
+FROM estado e, cidades c WHERE e.id = c.estado_id;
