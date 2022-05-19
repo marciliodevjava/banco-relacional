6a6df344110a4191a074d402c3e4a109e5bae894
marciliodevjava <marciliodevjava@gmail.com>
2022-05-19 08:12:42 -0300

    Atualização

--- inserirEmpresas.sql
+++ inserirEmpresas.sql
@@ -1,7 +1,19 @@
 INSERT INTO empresas (nome, cnpj)
 VALUES 
-    ('Bradesco', 9900990090900),
-    ('Vale', 989878978978998),
-    ('Cielo', 897987899787987);
+    ('Bradesco', 99009900909007),
+    ('Vale', 98987897897899),
+    ('Cielo', 89798789978798);
 
-    ALTER TABLE empresas
+ALTER TABLE EMPRESAS MODIFY CNPJ VARCHAR(14);
+
+SELECT * FROM empresas;
+SELECT * FROM cidades;
+
+INSERT INTO empresas_unidades (empresa_id, cidade_id, sede)
+VALUES 
+    (1, 1, 1),
+    (1, 2, 0),
+    (2, 1, 0),
+    (2, 2, 1);
+
+SELECT * FROM empresas_unidades;
